cc640a2a633309437f0d6b1cb02edb9457287339
Shin seongsu <tlstjdtn321@naver.com>
2020-01-07 22:16:24 +0900

    useState 사용법 및 static 사용

---
+++ chapter3/component/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import MyComponent from './MyComponent';
+//import Counter from './Counter';
+import Say from './say';
+
+const App = () => {
+  //return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+  
+  //return <Counter />;
+
+  return <Say />;
+};
+
+export default App;
---
+++ chapter3/component/src/Counter.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+/*    constructor(props) {
+        super(props);
+        // state 초깃값 설정
+        this.state = {
+            number: 0,
+            fixedNumber: 0
+        };
+    } */
+
+    state = {
+        number : 0,
+        fixedNumber : 0
+    };
+
+    render() {
+        const { number, fixedNumber } = this.state;
+
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값 : {fixedNumber}</h2>
+                <button
+                    onClick={() => {
+                  //      this.setState({ number : number + 1 });
+                  //      this.setState({ number: this.state.number + 1});
+
+                  //      this.setState(prevState => {
+                  //          return {
+                  //              number: prevState.number + 1
+                  //          };
+                  //      });
+                  //      this.setState(prevState => ({
+                  //          number: prevState.number + 1
+                  //      }));
+
+                          this.setState (
+                              {
+                                  number: number + 1
+                              },
+                              () => {
+                                  console.log('방금 setState가 호출되었습니다.');
+                                  console.log(this.state);
+                              }
+                          );
+                    }}
+                >
+                +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
