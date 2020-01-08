91913a212e3e81f65556cda7a89a3a30d6888ebe
Shin seongsu <tlstjdtn321@naver.com>
2020-01-08 22:05:10 +0900

    이벤트 처리

---
+++ chapter4/event/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import Say from './Say';
+//import EventPratice from './EventPratice';
+import EventPratice from './EventPratice1';
+
+const App = () => {
+//  return <Say />;
+  return <EventPratice />;
+}
+
+export default App;
---
+++ chapter4/event/src/Say.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+    const [message, setMessage] = useState('');
+    const onClickEnter = () => setMessage('안녕하세요!');
+    const onClickLeave = () => setMessage('안녕히 가세요!');
+
+    const [color, setColor] = useState('black');
+
+    return (
+        <div>
+            <button onClick={onClickEnter}>입장</button>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1 style={{color}}>{message}</h1>
+            <button style={{ color : 'red' }} onClick={ () => setColor('red')}>
+                빨강
+            </button>
+            <button style={{ color : 'yellow' }} onClick={ () => setColor('yellow')}>
+                노란색
+            </button>
+            <button style={{ color : 'blue' }} onClick={ () => setColor('blue')}>
+                파란색
+            </button>
+        </div>
+    );
+}
+
+export default Say;
