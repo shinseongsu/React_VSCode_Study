504bfa9374085eb6aaac62a02316623f87755235
Shin seongsu <tlstjdtn321@naver.com>
2020-01-12 19:40:00 +0900

    styling

---
+++ chapter8/hooks/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import Counter from './Counter';
+//import Info from './Info';
+//import Info from './Info1';
+//import Average from './Average';
+import Info from './Info2';
+
+const App = () => {
+  //return <Counter />;
+  //return <Info />;
+  //return <Info />;
+  //return <Average />;
+  return <Info />;
+};
+
+export default App;
