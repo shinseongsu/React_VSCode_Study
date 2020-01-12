235a2d38a1d83e1c5b6098b41f953bf0e4968127
Shin seongsu <tlstjdtn321@naver.com>
2020-01-12 21:22:25 +0900

    scss 공부

--- chapter9/styling/src/App.js
+++ chapter9/styling/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+//import SassComponent from './SassComponent';
+//import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+          <StyledComponent />
       </div>
     );
   }
