47945351e436ecc042ce3ff7eac8153a2b5122a8
Shin seongsu <tlstjdtn321@naver.com>
2020-01-23 23:39:51 +0900

    CounterContainer

--- chapter17/react-redux-tutorial/src/containers/CounterContainer.js
+++ chapter17/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
     return (
@@ -8,20 +9,33 @@ const CounterContainer = ({ number, increase, decrease }) => {
     );
 };
 
+/*
 const mapStateToProps = state => ({
     number: state.counter.number
 });
 
 const mapDispatchToProps = dispatch => ({
     increase : () => {
-        console.log('increase');
+        dispatch(increase());
     },
     decrease : () => {
-        console.log('decrease');
+        dispatch(decrease());
     },
 });
 
 export default connect(
     mapStateToProps,
     mapDispatchToProps,
+)(CounterContainer);
+
+*/
+
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch => ({
+        increase: () => dispatch(increase()),
+        decrease: () => dispatch(decrease()),
+    }),
 )(CounterContainer);
