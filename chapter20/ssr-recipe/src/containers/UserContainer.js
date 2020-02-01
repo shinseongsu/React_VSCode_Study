371ffc9109c0496b110ef11825d070e14f9727a7
Shin seongsu <tlstjdtn321@naver.com>
2020-02-01 18:20:29 +0900

    서버 사이드 렌더링

--- chapter20/ssr-recipe/src/containers/UserContainer.js
+++ chapter20/ssr-recipe/src/containers/UserContainer.js
@@ -1,21 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import User from '../components/User';
-import { Preloader } from '../lib/PreloadContext';
+import { usePreloader } from '../lib/PreloadContext';
 import { getUser } from '../modules/users';
 
 const UserContainer = ({ id }) => {
     const user = useSelector(state => state.users.user);
     const dispatch = useDispatch();
 
+    usePreloader(() => dispatch(getUser(id)));
     useEffect(() => {
         if(user && user.id === parseInt(id, 10)) return;
         dispatch(getUser(id));
     }, [dispatch, id, user]);
 
-    if(!user) {
-        return <Preloader resolve={() => dispatch(getUser(id))} />;
-    }
+    if(!user) return null;
     return <User user={user} />;
 };
 
