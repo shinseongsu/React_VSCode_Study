7223c2ce3eb27dbc0ce5715d7f32e91aeceee560
Shin seongsu <tlstjdtn321@naver.com>
2020-02-06 20:42:07 +0900

    mongoose

--- chapter21/blog-backend/src/api/posts/index.js
+++ chapter21/blog-backend/src/api/posts/index.js
@@ -1,15 +1,7 @@
-const Router = require('koa-router');
-const postsCtrl = require('./posts.ctrl');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
-
-const printInfo = ctx => {
-    ctx.body = {
-        method: ctx.method,
-        path: ctx.path,
-        params: ctx.params,
-    };
-};
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
@@ -18,4 +10,4 @@ posts.delete('/:id', postsCtrl.remove);
 posts.put('/:id', postsCtrl.replace);
 posts.patch('/:id', postsCtrl.update);
 
-module.exports = posts;
+export default posts;
