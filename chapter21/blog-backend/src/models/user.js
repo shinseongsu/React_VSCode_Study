b02908ffe4268db71ca1d162a8c97f7bd43e6d96
Shin seongsu <tlstjdtn321@naver.com>
2020-02-10 23:42:35 +0900

    jwt

--- chapter21/blog-backend/src/models/user.js
+++ chapter21/blog-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
     username: String,
@@ -26,5 +27,21 @@ UserSchema.methods.serialize = function() {
     return data;
 }
 
+UserSchema.methods.generateToken = function() {
+    const token = jwt.sign(
+        {
+            _id: this.id,
+            username: this.username,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '7d',
+        },
+    );
+    return token;
+};
+
 const User = mongoose.model('User', UserSchema);
 export default User;
+
+
